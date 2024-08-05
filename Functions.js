829edb3255f6211723692ab93efefa91871d558e
Remvolt <ozzytygon@gmail.com>
2024-08-04 20:44:12 -0400

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,75 @@
+function DisplayChat1(){
+
+    document.getElementById('chat-1').style.display="block";
+    document.getElementById('chat-2').style.display="none";
+    document.getElementById('chat-3').style.display="none";
+    document.getElementById('chat-4').style.display="none";
+    document.getElementById('chat-5').style.display="none";
+    document.getElementById('chat-6').style.display="none";
+
+}
+
+function DisplayChat2(){
+
+    document.getElementById('chat-1').style.display="none";
+    document.getElementById('chat-2').style.display="block";
+    document.getElementById('chat-3').style.display="none";
+    document.getElementById('chat-4').style.display="none";
+    document.getElementById('chat-5').style.display="none";
+    document.getElementById('chat-6').style.display="none";
+    document.getElementById('chat-ping-2').style.display="none";
+    document.getElementById('chat-ping-2-mobile').style.display="none";
+    document.getElementById('unread-user-2').style.fontWeight="normal";
+    document.getElementById('unread-user-2-mobile').style.fontWeight="normal"; 
+    document.getElementById('unread-time-2').style.fontWeight="normal";
+    document.getElementById('unread-time-2-mobile').style.fontWeight="normal";
+}
+
+function DisplayChat3(){
+
+    document.getElementById('chat-1').style.display="none";
+    document.getElementById('chat-2').style.display="none";
+    document.getElementById('chat-3').style.display="block";
+    document.getElementById('chat-4').style.display="none";
+    document.getElementById('chat-5').style.display="none";
+    document.getElementById('chat-6').style.display="none";
+    
+}
+
+function DisplayChat4(){
+
+    document.getElementById('chat-1').style.display="none";
+    document.getElementById('chat-2').style.display="none";
+    document.getElementById('chat-3').style.display="none";
+    document.getElementById('chat-4').style.display="block";
+    document.getElementById('chat-5').style.display="none";
+    document.getElementById('chat-6').style.display="none";
+    document.getElementById('chat-ping-4').style.display="none";
+    document.getElementById('chat-ping-4-mobile').style.display="none";
+    document.getElementById('unread-user-4').style.fontWeight="normal";
+    document.getElementById('unread-user-4-mobile').style.fontWeight="normal"; 
+    document.getElementById('unread-time-4').style.fontWeight="normal";
+    document.getElementById('unread-time-4-mobile').style.fontWeight="normal"; 
+}
+
+function DisplayChat5(){
+
+    document.getElementById('chat-1').style.display="none";
+    document.getElementById('chat-2').style.display="none";
+    document.getElementById('chat-3').style.display="none";
+    document.getElementById('chat-4').style.display="none";
+    document.getElementById('chat-5').style.display="block";
+    document.getElementById('chat-6').style.display="none";
+    
+}
+
+function DisplayChat6(){
+
+    document.getElementById('chat-1').style.display="none";
+    document.getElementById('chat-2').style.display="none";
+    document.getElementById('chat-3').style.display="none";
+    document.getElementById('chat-4').style.display="none";
+    document.getElementById('chat-5').style.display="none";
+    document.getElementById('chat-6').style.display="block";
+    
+}
